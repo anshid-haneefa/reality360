d6621f4c0bcb5946bd6eb34ab34cb4e3c5bc2b05
anshid <anshidhaneefa710@gmail.com>
2026-02-16 19:29:16 +0530

    opacity adjust

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -107,7 +107,7 @@ export function HeroSection() {
       >
         <div className="absolute inset-0 scale-110">
           <Image
-            src="/images/hero-vr3.png"
+            src="/images/hero-vr.jpg"
             alt=""
             fill
             className="object-cover opacity-50"
